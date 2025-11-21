525530d3f800255c85cd36013278c4b5bb4b6855
Вероника Боронина <nboronina0512@gmail.com>
2025-11-21 20:41:56 +0300

    hw updated

--- hw_1.py
+++ hw_1.py
@@ -1,5 +1,5 @@
 weight, height, age = 80, 180, 25
 
-IMT = weight / (height)**2 # У вас ошибка! Вы не перевели из см в м. Поделите на 100
+IMT = weight / (height/100)**2
 
 print(f"Patient`s body mnass index: {IMT}")
